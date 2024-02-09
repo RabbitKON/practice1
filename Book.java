08654e4229791bd8ee363b59c1ee9b8ebec60097
RabbitKON <kpypk666@gmail.com>
2024-02-09 19:58:54 +0900

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,32 @@
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+public class Book extends TangibleAsset {
+  String isbn;
+  public Book(String name, int price, String color, String isbn) {
+    super(name, price, color);
+    this.isbn = isbn;
+  }
+  public String getIsbn() { return this.isbn; }
+}~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+Book.java [unix] (08:59 01/01/1970)                                      0,1 All
+-- INSERT --
+
